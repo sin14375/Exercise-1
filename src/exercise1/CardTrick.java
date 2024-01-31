563348a621321359b55e0afcf211428a609d590f
Harshdeep Singh <sin14375@sheridancollege.ca>
2024-01-30 19:37:19 -0500

    Fourth Commit with Done Comment

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -64,6 +64,7 @@ public class CardTrick
     private static void printInfo() 
     {
     
+        System.out.println("I'm done!");
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
